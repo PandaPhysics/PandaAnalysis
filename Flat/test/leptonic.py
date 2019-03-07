d74438c75a8fb803125a270862416fbd3faf36b4
GuillelmoGomezCeballos <ceballos@cern.ch>
2019-03-07 08:20:41 -0500

    improved leptonic macro

--- Flat/test/leptonic.py
+++ Flat/test/leptonic.py
@@ -22,12 +22,13 @@ import PandaAnalysis.T3.job_utilities as utils
 
 Load('PandaAnalyzer')
 
+year = 2018
 analysis = vv(True)
 analysis.inpath = torun
 analysis.outpath = output
 analysis.datapath = getenv('CMSSW_BASE') + '/src/PandaAnalysis/data/'
 analysis.isData = True
-utils.set_year(analysis, 2018)
+utils.set_year(analysis, year)
 analysis.processType = utils.classify_sample(torun, analysis.isData)
 
 print "Process: ",analysis.processType
@@ -40,8 +41,14 @@ skimmer.firstEvent=0
 skimmer.lastEvent=10000
 skimmer.isData=analysis.isData
 if skimmer.isData:
-    with open(getenv('CMSSW_BASE')+'/src/PandaAnalysis/data/certs/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt') as jsonFile:
-    #with open(getenv('CMSSW_BASE')+'/src/PandaAnalysis/data/certs/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt') as jsonFile:
+    fileName = ''
+    if year==2018:
+        fileName = getenv('CMSSW_BASE')+'/src/PandaAnalysis/data/certs/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
+    elif year==2017:
+        fileName = getenv('CMSSW_BASE')+'/src/PandaAnalysis/data/certs/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+    elif year==2016:
+        fileName = getenv('CMSSW_BASE')+'/src/PandaAnalysis/data/certs/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+    with open(fileName) as jsonFile:
         payload = json.load(jsonFile)
         for run,lumis in payload.iteritems():
             for l in lumis:
