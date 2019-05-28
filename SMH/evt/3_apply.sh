7cc0119205fde4d442262bc4697c62979719bf54
Benedikt Maier <benedikt.maier@cern.ch>
2019-05-28 08:46:16 -0400

    darkg

--- SMH/evt/3_apply.sh
+++
@@ -1,5 +0,0 @@
-for i in /data/t3home000/hbb/zhnn/2018_v8/sr/*root; do
-
-    python /home/bmaier/cms/Hbb/2018/CMSSW_10_3_1/src/PandaAnalysis/SMH/evt/infer_on_root.py  --ifile $i --json /home/bmaier/cms/Hbb/2018/CMSSW_10_3_1/src/PandaAnalysis/SMH/evt/root.json --model /home/bmaier/cms/Hbb/2018/SubTLEnet/train/smh/models/evt/2018_v8/weights.h5
-
-done
